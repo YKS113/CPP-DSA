64b577fa47c5dd812cd3329d6ad44ddd08649a61
YashKishorSonar <yksworks113@gmail.com>
2023-08-02 23:53:16 +0530

    nc

--- p20_linked-list.cpp
+++ p20_linked-list.cpp
@@ -3,6 +3,8 @@
 //insertion/deletion is easy beacuse it does not need to shift spaces for these operations(like in array) to maintain order
 //disadvantage- 1. traversal is hard to reach nth node, we have to traverse from zeroth node to nth node through every other intermediate node; While in array if we want to access nth element, we do simple arithmatic &(0th element) + n*4 to reach the nth element                                                                          2. since, it also stores pointers, it takes more memory
 
+//Types-
+//1.Singly linked list- every node has data and ptr to next node, ptr of last/tail node is NULL 
 //creating a node
 /*
 #include<iostream>
@@ -125,7 +127,8 @@ cout<<endl;
 }
 */
 
-//Doubly linked list
+//2.Doubly linked list- every node has data, ptr to next node and a ptr to prev node, next ptr of last/tail and prev ptr of first/head node are NULL 
+/*
 #include<iostream>
 using namespace std;
 class Node{
@@ -244,3 +247,106 @@ int main()
     cout<<endl;
 
 }
+*/
+
+//1.Circular linked list- similar to Singly LL, but ptr of tail node points to head node 
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+void insertAtHead(Node* &head,Node* &tail, int data){ 
+    Node* temp= new Node(data);
+    temp->next=head;
+    tail->next=temp;
+    head=temp;                 
+    }  
+void insertAtTail(Node* &head,Node* &tail, int data){ 
+    Node* temp= new Node(data);
+    temp->next=head;
+    tail->next=temp;
+    tail=temp;                 
+    }
+void insertAtMid(Node* &head, Node* &tail, int data, int n){ //insert at nth node
+    if(n==0){//edge case, since for n=0, we dont have beforemid
+        insertAtHead(head,tail,data);
+        return;
+    }
+    if(n==length(head)){
+        insertAtTail(head,tail,data);
+        return;
+    }
+    Node* temp= new Node(data);
+    Node* mid=head;
+    Node* beforeMid;
+    for(int i=0;i<n;i++){
+        beforeMid=mid;
+        mid=mid->next;
+    }
+    beforeMid->next=temp;
+    temp->next=mid;
+}
+void printNode(Node* head){
+    Node* temp=head;
+    cout<<temp->data<<" ";
+    temp=temp->next;
+while(temp!=head){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+}
+}
+int length(Node* head){
+        Node* temp=head->next;//we've counted first node already by giving count=1
+        int count=1;
+        while(temp!=head){
+            count++;
+            temp=temp->next;
+        }
+        return count;    
+    
+    }
+    
+};
+int main()
+{
+    Node* node1=new Node(50);
+    Node* head=node1;
+    Node* tail=node1;
+    head->insertAtHead(head,tail,40);
+    head->printNode(head);
+    cout<<endl;
+    head->insertAtTail(head,tail,60);
+    head->printNode(head);
+    cout<<endl;
+
+    head->insertAtTail(head,tail,70);
+    head->insertAtTail(head,tail,80);
+    head->insertAtTail(head,tail,90);
+    head->insertAtTail(head,tail,100);
+
+    head->insertAtMid(head,tail,75,4);
+    head->printNode(head);
+    cout<<endl;
+    head->insertAtMid(head,tail,30,0);
+    head->printNode(head);
+    cout<<endl;
+    head->insertAtMid(head,tail,110,9);
+    head->printNode(head);
+    cout<<endl;
+}
+
+//Similarly can be solved for Doubly Circular linked list- similar to Singly Circular LL, but next ptr of tail node points to head node and prev ptr of head node points to tail node
+
+//P-1 Reverse a linked list 
+//LC- 206
+//TC- O(n), SC-O(1)
+
+//P-2 Middle of LL
+//LC- 876
+//M-1 simple TC- O(n)
+//M-2 Fast pointer approach-- It involves two ptrs a fast and slow, fast moves two blocks while slow moves one block in one iteration, by the time fast ptr completes the traversal, slow ptr reaches mid.  TC- O(n/2)
