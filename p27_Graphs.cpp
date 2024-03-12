1e1f8b3974843dcd01ec8ff5c64d1929aa17142e
YashKishorSonar <yksworks113@gmail.com>
2024-03-12 23:48:44 +0530

    12/3/24

---
+++ p27_Graphs.cpp
@@ -0,0 +1,17 @@
+/*
+1. Graph is a collection of Nodes connected by Edges
+2. Types of graphs- 
+Directed : edges are single directional
+Undirected : edges are Bi-directional
+
+3. Degree of a Node(for Undirected graph)- no. of other nodes directly connected to that node/no of edges attached to that node.
+for directed graph, 
+Indegree- no. of edges pointing towards the node
+Outdegree- no. of edges pointing out from that node
+
+4. Weighted graph- a graph with some weight value on every edge, every edge can have same/different weight values, for normal unweighted graphs,weights are assumed to be 1
+
+5. Path- a sequence of nodes, where each node in the path is visited exactly once
+6. Cyclic graph-https://www.geeksforgeeks.org/what-is-cyclic-graph/
+7. Graph representation- https://www.geeksforgeeks.org/graph-and-its-representations/
+*/
